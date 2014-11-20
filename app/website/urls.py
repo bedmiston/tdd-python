2570a123b4742a6bc291360dc5622c4d025ebee0
Brendon Edmiston <bedmiston@gmail.com>
2014-11-20 12:05:33 -0600

    First unit test and url mapping, dummy view.

--- app/website/urls.py
+++ app/website/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
