b9065b3c92aefaba43944277ec884d96512ba5dc
Brendon Edmiston <bedmiston@gmail.com>
2014-11-21 12:37:31 -0600

    Add List model and associate each item with a list.

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -27,8 +27,9 @@ class LiveViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_displays_all_list_tems(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        itemlist = List.objects.create()
+        Item.objects.create(text='itemey 1', list=itemlist)
+        Item.objects.create(text='itemey 2', list=itemlist)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -36,16 +37,24 @@ class LiveViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        itemlist = List()
+        itemlist.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = itemlist
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = itemlist
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, itemlist)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -53,7 +62,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, itemlist)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, itemlist)
 
 
 class NewListTest(TestCase):
