9b1b9a33520fd45990457c8c568ad9a6acc5a47c
Brendon Edmiston <bedmiston@gmail.com>
2014-11-24 07:40:41 -0600

    Refactor fabric functional test tasks to remove code duplication.

--- fabfile.py
+++ fabfile.py
@@ -143,16 +143,20 @@ def figrun(command):
     with cd(env.vagrant_folder):
         run("fig %s" % command)
 
+def functest(command):
+    with lcd("app"):
+        with shell_env(DJANGO_SETTINGS_MODULE="website.settings.test"):
+            with shell_env(DJANGO_SECRET_KEY='l2m12=spld!a^m30@%gkvk*)f*x(wh18v70ch04mpnnt%!-h7t'):
+                local("python manage.py test functional_tests" + command)
 
 def ft():
-    with lcd("app/"):
-        with shell_env(DJANGO_SETTINGS_MODULE="website.settings.test"):
-            with shell_env(DJANGO_SECRET_KEY='l2m12=spld!a^m30@%gkvk*)f*x(wh18v70ch04mpnnt%!-h7t'):
-                local("python manage.py test functional_tests")
+    functest("")
 
+def ftl():
+    functest(" --liveserver=33.33.33.33")
 
 def fts(test):
-    with lcd("app/"):
-        with shell_env(DJANGO_SETTINGS_MODULE="website.settings.test"):
-            with shell_env(DJANGO_SECRET_KEY='l2m12=spld!a^m30@%gkvk*)f*x(wh18v70ch04mpnnt%!-h7t'):
-                local("python manage.py test functional_tests.%s" % (test,))
+    functest(".%s" % (test,))
+
+def ftls(test):
+    functest(".%s --liveserver=33.33.33.33" % (test,))
