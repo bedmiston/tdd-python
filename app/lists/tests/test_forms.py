bfb9f7a9034d5e2103d86f35d344cf982bb80908
Brendon Edmiston <bedmiston@gmail.com>
2015-01-09 11:24:37 -0600

    Add a save method to the form.

--- app/lists/tests/test_forms.py
+++ app/lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import EMPTY_LIST_ERROR, ItemForm
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -14,3 +15,11 @@ class ItemFormTest(TestCase):
         self.assertEqual(
             form.errors['text'],
             [EMPTY_LIST_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        itemlist = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=itemlist)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, itemlist)
